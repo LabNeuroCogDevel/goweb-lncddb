9cc73d906871134176e8e9d49f11809c06f952ad
Will Foran <willforan@gmail.com>
2016-08-02 16:23:33 -0400

    add elm submodule

--- readme.md
+++ readme.md
@@ -1,1 +1,13 @@
- GOPATH="$GOPATH:$HOME/src/dbexperements/go/" go test
+# 
+
+## dependencies
+```
+git submodule foreach git pull
+go get github.com/LabNeuroCogDevel/ldapauth
+```
+
+## local devel notes
+
+GOPATH="$GOPATH:$HOME/src/dbexperements/go/" go test
+GOPATH="$GOPATH:$HOME/src/dbexperements/go/" go build && ./web
+
--- server.go
+++ server.go
@@ -10,7 +10,11 @@ import (
   "flag"
   "fmt"
   "strings"
- // "github.com/gorilla/mux" // we need regexp routes
+  //"ldapauth"
+  "github.com/LabNeuroCogDevel/ldapauth"
+  //"github.com/LabNeuroCogDevel/lncdcal"
+  "github.com/dgrijalva/jwt-go"
+
 )
 
 var serveport string = "3001"
@@ -22,13 +26,45 @@ var proxyuri *url.URL
 
 type Proxy struct{
   proxy *httputil.ReverseProxy
-  proxypath string
+  proxyroute string
 }
 func (p *Proxy)  handle (w http.ResponseWriter, r *http.Request) {
     //http.HandlerFunc 
-    r.URL.Path = strings.Replace(r.URL.Path,p.proxypath,"",1)
+    r.URL.Path = strings.Replace(r.URL.Path,p.proxyroute,"",1)
     log.Printf(r.URL.Path)
     p.proxy.ServeHTTP(w,r)
+}
+
+func login (w http.ResponseWriter, r *http.Request, sstr string) {
+    r.ParseForm()
+
+    if ( len(r.Form["user"])==0 || len(r.Form["pass"])==0 ) {
+       http.Error(w, "bad input", http.StatusNotAcceptable)
+       return
+    }
+
+    // check ldap and hardcoded test
+    isauth := false
+    if ( r.Form["user"][0] == "test" &&  r.Form["pass"][0] == "test" ){
+     isauth = true
+    } else {
+     isauth = ldapauth.IsAuth(r.Form["user"][0],r.Form["pass"][0])
+    }
+
+    // return authentication
+    if ! isauth {
+       http.Error(w, "Not authorized", http.StatusUnauthorized)
+       return
+    } else {
+       token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+             "user": r.Form["user"][0],
+             "role": "lncd" })
+       tokenString, err := token.SignedString([]byte(sstr))
+       if err == nil {
+         fmt.Fprintf(w, "%s", tokenString)
+       }
+       fmt.Println(err)
+    }
 }
 
 //////////////////////
@@ -37,8 +73,10 @@ func main(){
 
   serveport   := flag.Int   ("port"  ,3001,                 "port to run proxy server on")
   proxyuristr := flag.String("uri"   ,"http://0.0.0.0:3000","uri of server to be proxied")
-  proxypath   := flag.String("ppath", "db/",                 "db route")
-  staticpath  := flag.String("static","./static",           "filesystem path to static web assests")
+  proxyroute  := flag.String("ppath", "db/",                "db route")
+  staticpath  := flag.String("static","./elm-server-interface","filesystem path to static web assests")
+  loginroute  := flag.String("login","login",               "where to send auth string")
+  sstr        := flag.String("secret","secret",             "hash for jwt")
   flag.Parse()
 
   proxyuri,err := url.Parse(*proxyuristr)
@@ -46,7 +84,7 @@ func main(){
 
   // add ":" to port to get eg ":3001"
   port := fmt.Sprintf(":%d",*serveport)
-  log.Printf("Serving %s on %s, proxy @ '/%s'",proxyuri,port,*proxypath)
+  log.Printf("Serving %s on %s, proxy @ '/%s', login @ '/%s'",proxyuri,port,*proxyroute, *loginroute)
 
 
   // tried to do without type+handler
@@ -54,12 +92,20 @@ func main(){
   //proxy := httputil.NewSingleHostReverseProxy(proxyuri)
 
   p := &Proxy{ proxy: httputil.NewSingleHostReverseProxy(proxyuri),
-              proxypath: *proxypath }
+              proxyroute: *proxyroute }
 
 
-  //r:=mux.NewRouter()
-  http.HandleFunc("/"+*proxypath,p.handle)
+
+  // login
+  loginsstr:=func(w http.ResponseWriter, r *http.Request) { login(w,r,*sstr) }
+  http.HandleFunc("/"+*loginroute,loginsstr)
+
+  // database proxy
+  http.HandleFunc("/"+*proxyroute,p.handle)
+  // static assests
   http.Handle("/",http.FileServer(http.Dir(*staticpath)))
+
+  // all done
   log.Fatal(http.ListenAndServe(port,nil))
 
 
