3fc25aad6395d1a7adf7c85bcf2ceeacf7483723
WillForan <will.foran@gmail.com>
2016-09-07 11:52:17 -0400

    add dbrole and more readme

--- readme.md
+++ readme.md
@@ -1,4 +1,45 @@
-# 
+# web wrapper for postgrest
+
+Assume a postgresql database with login-only role `web` and select,update,delete role `lncd`:
+
+```
+postgrest -j abcd -m 300 postgres://postgres@localhost:5432/lncddb -a web
+./web -secret abcd -dbrole lncd
+
+# test
+## should fail
+curl localhost:3000/person |jq
+## success?
+curl localhost:3000/person -H "Range-Unit: items" -H "Range: 0-0" -H "Authorization: Bearer $(curl  localhost:3001/login -d user=test -d pass=test )"|jq
+```
+
+
+## usage
+```
+./web -help
+```
+
+> Usage of ./web:
+>   -dbrole string
+>        database user/role for token (default "lncd")
+>   -login string
+>     	where to send auth string (default "login")
+>   -port int
+>     	port to run proxy server on (default 3001)
+>   -ppath string
+>     	db route (default "db/")
+>   -secret string
+>     	hash for jwt (default "secret")
+>   -static string
+>     	filesystem path to static web assests (default "./elm-server-interface")
+>   -uri string
+>     	uri of server to be proxied (default "http://0.0.0.0:3000")
+
+## usefulness
+get a token to access database
+```
+curl  http://0.0.0.0:3001/login -d 'user=test' -d pass=test
+```
 
 ## dependencies
 ```
@@ -11,3 +52,7 @@ go get github.com/LabNeuroCogDevel/ldapauth
 GOPATH="$GOPATH:$HOME/src/dbexperements/go/" go test
 GOPATH="$GOPATH:$HOME/src/dbexperements/go/" go build && ./web
 
+## related
+* bitbucket.org/LabNeuroCogDevel/postgresql-export
+* github.com/LabNeuroCogDevel/elm-server-interface
+* github.com/LabNeuroCogDevel/ldapauth
--- server.go
+++ server.go
@@ -31,11 +31,13 @@ type Proxy struct{
 func (p *Proxy)  handle (w http.ResponseWriter, r *http.Request) {
     //http.HandlerFunc 
     r.URL.Path = strings.Replace(r.URL.Path,p.proxyroute,"",1)
+    //TODO: if we are adding or removing a visit
+    // modify the google calendar
     log.Printf(r.URL.Path)
     p.proxy.ServeHTTP(w,r)
 }
 
-func login (w http.ResponseWriter, r *http.Request, sstr string) {
+func login (w http.ResponseWriter, r *http.Request, sstr string,dbrole string) {
     r.ParseForm()
 
     if ( len(r.Form["user"])==0 || len(r.Form["pass"])==0 ) {
@@ -58,7 +60,7 @@ func login (w http.ResponseWriter, r *http.Request, sstr string) {
     } else {
        token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
              "user": r.Form["user"][0],
-             "role": "lncd" })
+             "role": dbrole })
        tokenString, err := token.SignedString([]byte(sstr))
        if err == nil {
          fmt.Fprintf(w, "%s", tokenString)
@@ -77,6 +79,7 @@ func main(){
   staticpath  := flag.String("static","./elm-server-interface","filesystem path to static web assests")
   loginroute  := flag.String("login","login",               "where to send auth string")
   sstr        := flag.String("secret","secret",             "hash for jwt")
+  dbrole      := flag.String("dbrole","lncd",               "database user/role for token")
   flag.Parse()
 
   proxyuri,err := url.Parse(*proxyuristr)
@@ -97,7 +100,8 @@ func main(){
 
 
   // login
-  loginsstr:=func(w http.ResponseWriter, r *http.Request) { login(w,r,*sstr) }
+  // use the provided -login path to make a token with the user and -dbuser for postgrest
+  loginsstr:=func(w http.ResponseWriter, r *http.Request) { login(w,r,*sstr,*dbrole) }
   http.HandleFunc("/"+*loginroute,loginsstr)
 
   // database proxy
